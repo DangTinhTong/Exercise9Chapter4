d1364fcce00d381d659d8a2ab9cae83d8e37adf6
admin <dangtinhtong@gmail.com>
2017-11-22 16:00:05 +0700

    Initial Commit

---
+++ Exercise9/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Exercise9
+//
+//  Created by admin on 11/22/17.
+//  Copyright © 2017 admin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
